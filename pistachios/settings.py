a81a573f5da60e206576e20b261d8f3b48e4813f
Natash <nvalentejcik@gmail.com>
2023-01-02 23:28:24 +0300

    first message

--- pistachios/settings.py
+++ pistachios/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zm4#f6d_8#vv)iu)97zpmg64skq9=v4955h%cid676kk@a0liu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
